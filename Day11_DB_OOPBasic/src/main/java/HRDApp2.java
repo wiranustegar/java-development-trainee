8c80351706864777f51bbaa7253f9a98f11304bf
Wiranus Tegar <tegarpriatna123@gmail.com>
2023-05-24 20:46:28 +0700

    fixing

--- Day11_DB_OOPBasic/src/main/java/HRDApp2.java
+++ Day11_DB_OOPBasic/src/main/java/HRDApp2.java
@@ -83,18 +83,12 @@ public class HRDApp2 {
 
     public static void main(String[] args) {
         System.out.println("=========EMPLOYEE PORTAL=========");
-        System.out.println("1). CARI EMPLOYEE" +
-                "");
-        System.out.println("2). INSERT EMPLOYEE" +
-                "");
-        System.out.println("3). UPDATE EMPLOYEE" +
-                "");
-        System.out.println("4). TAMPILKAN SEMUA EMPLOYEE" +
-                "");
-        System.out.println("5). CARI NAMA EMPLOYEE" +
-                "");
-        System.out.println("6). DELETE EMPLOYEE" +
-                "");
+        System.out.println("1). CARI EMPLOYEE");
+        System.out.println("2). INSERT EMPLOYEE");
+        System.out.println("3). UPDATE EMPLOYEE");
+        System.out.println("4). TAMPILKAN SEMUA EMPLOYEE");
+        System.out.println("5). CARI NAMA EMPLOYEE");
+        System.out.println("6). DELETE EMPLOYEE");
         System.out.println("7). EXIT");
 
         while (true){
@@ -102,14 +96,12 @@ public class HRDApp2 {
             int pilih = Integer.parseInt(scan.nextLine());
 
             if (pilih==1){
-                System.out.println("=========CARI EMPLOYEE" +
-                        "=========");
+                System.out.println("=========CARI EMPLOYEE=========");
                 System.out.print("NIP: ");
                 int nip = Integer.parseInt(scan.nextLine());
                 cariEmployee(nip);
             } else if (pilih==2) {
-                System.out.println("=========INSERT EMPLOYEE" +
-                        "=========");
+                System.out.println("=========INSERT EMPLOYEE=========");
                 System.out.print("Nama: ");
                 String nama = scan.nextLine();
                 System.out.print("NIP: ");
@@ -118,8 +110,7 @@ public class HRDApp2 {
                 double gaji = Double.parseDouble(scan.nextLine());
                 insertEmployee(nip,nama,gaji);
             } else if (pilih==3) {
-                System.out.println("=========UPDATE EMPLOYEE" +
-                        "=========");
+                System.out.println("=========UPDATE EMPLOYEE=========");
                 System.out.print("NIP yang akan diupdate: ");
                 int nip = Integer.parseInt(scan.nextLine());
                 System.out.print("Nama: ");
@@ -128,12 +119,10 @@ public class HRDApp2 {
                 double gaji = Double.parseDouble(scan.nextLine());
                 updateEmployee(nama,nip,gaji);
             } else if (pilih==4) {
-                System.out.println("=========TAMPLIKAN SEMUA EMPLOYEE" +
-                        "=========");
+                System.out.println("=========TAMPLIKAN SEMUA EMPLOYEE=========");
                 cariSemuaEmployee();
             } else if (pilih==5) {
-                System.out.println("=========CARI EMPLOYEE" +
-                        " BY NAME=========");
+                System.out.println("=========CARI EMPLOYEE BY NAME=========");
                 System.out.print("NAMA: ");
                 String nama = scan.nextLine();
                 findByFirstName(nama);
